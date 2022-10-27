65aaaa4d7256b0497f106fcad116ed4760639ccc
BT_3W2_22 <nathanz@nycstudents.net>
2022-10-27 09:21:32 -0400

    update

--- src/DogRunner.java
+++ src/DogRunner.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class DogRunner {
+public class DogRunner {
+    public static void main(String [] args) {
+        Dog dog1 = new Dog("Spot", 10, false);
+        Dog dog2 = new Dog("Petunia", 12, true);
+        Dog dog3 = new Dog("Lucky", 3, false);
+        Dog dog4 = new Dog("Spot", 5, true);
+        Dog dog5 = new Dog("Petunia", 12, true);
+        Dog dog6 = dog1;
+        Dog dog7 = dog2;
+        Dog dog8 = dog6;
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+        System.out.println(dog3);
+        System.out.println(dog4);
+        System.out.println(dog5);
+        System.out.println(dog6);
+        System.out.println(dog7);
+        System.out.println(dog8);
+    }
 }
